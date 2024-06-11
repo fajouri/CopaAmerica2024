b40b1fac29a9971dd8ed18afb62eb458e0746963
Yamil Emmanuel Fajouri <fajouri.yamil@gmail.com>
2024-06-11 10:44:02 +0200

    Update Program.cs
    Included the Knockout stage

--- CopaAmerica2024Prediction/Program.cs
+++ CopaAmerica2024Prediction/Program.cs
@@ -1,4 +1,4 @@
-﻿public class Team
+public class Team
 {
 	public string Name { get; set; }
 	public double EloRating { get; set; }
@@ -47,20 +47,80 @@ public class Program
 
 	private static string SimulateTournament()
 	{
-		List<string> remainingTeams = [.. EloRatings.Keys];
+		var groups = CreateGroups();
+		var groupWinners = SimulateGroupStage(groups);
+		var knockoutStage = SimulateKnockoutStage(groupWinners);
+		return knockoutStage;
+	}
 
-		while (remainingTeams.Count > 1)
+	private static List<List<string>> CreateGroups()
+	{
+		var pot1 = new List<string> { "Argentina", "Mexico", "USA", "Brazil" };
+		var pot2 = new List<string> { "Uruguay", "Colombia", "Ecuador", "Peru" };
+		var pot3 = new List<string> { "Chile", "Panama", "Venezuela", "Paraguay" };
+		var pot4 = new List<string> { "Jamaica", "Bolivia", "Canada", "Costa Rica" };
+
+		var groups = new List<List<string>>();
+
+		for (int i = 0; i < 4; i++)
 		{
-			List<string> nextRoundTeams = [];
-			for (int i = 0; i < remainingTeams.Count; i += 2)
-			{
-				string winner = SimulateMatch(remainingTeams[i], remainingTeams[i + 1]);
-				nextRoundTeams.Add(winner);
-			}
-			remainingTeams = nextRoundTeams;
+			groups.Add(new List<string> { pot1[i], pot2[i], pot3[i], pot4[i] });
 		}
 
-		return remainingTeams[0];
+		return groups;
+	}
+
+	private static List<string> SimulateGroupStage(List<List<string>> groups)
+	{
+		var advancingTeams = new List<string>();
+
+		foreach (var group in groups)
+		{
+			var groupStandings = new Dictionary<string, int>();
+
+			foreach (var team in group)
+			{
+				groupStandings[team] = 0;
+			}
+
+			for (int i = 0; i < group.Count; i++)
+			{
+				for (int j = i + 1; j < group.Count; j++)
+				{
+					var winner = SimulateMatch(group[i], group[j]);
+					groupStandings[winner]++;
+				}
+			}
+
+			var sortedStandings = groupStandings.OrderByDescending(x => x.Value).ToList();
+			advancingTeams.Add(sortedStandings[0].Key);
+			advancingTeams.Add(sortedStandings[1].Key);
+		}
+
+		return advancingTeams;
+	}
+
+	private static string SimulateKnockoutStage(List<string> advancingTeams)
+	{
+		var quarterFinals = new List<(string, string)>
+			{
+				(advancingTeams[0], advancingTeams[5]),
+				(advancingTeams[4], advancingTeams[1]),
+				(advancingTeams[2], advancingTeams[7]),
+				(advancingTeams[6], advancingTeams[3])
+			};
+
+		var semiFinals = new List<string>();
+
+		foreach (var match in quarterFinals)
+		{
+			semiFinals.Add(SimulateMatch(match.Item1, match.Item2));
+		}
+
+		var final = SimulateMatch(semiFinals[0], semiFinals[1]);
+		var thirdPlace = SimulateMatch(semiFinals[2], semiFinals[3]);
+
+		return SimulateMatch(final, thirdPlace);
 	}
 
 	private static string SimulateMatch(string teamA, string teamB)
